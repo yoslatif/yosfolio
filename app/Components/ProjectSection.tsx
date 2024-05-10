2433d7318437e6856948578319aa1db82e8c4424
yoslatif <yoslatif@gmail.com>
2024-05-09 17:53:06 -0700

    added githublinks

--- app/Components/ProjectSection.tsx
+++ app/Components/ProjectSection.tsx
@@ -17,7 +17,8 @@ const ProjectSection = () => {
       description: "Parmazon Prime, built with a PostgreSQL Flask backend and a React/Redux frontend, is a delectable twist on the shopping experience, bringing the charm of Amazon to the world of artisanal cheeses. This platform allows users to easily explore and order an array of gourmet options with the same ease and reliability as their favorite online marketplace.",
       technologies: ["Flask", "React", "SQL"],
       image: "/ParmazonImg.png",
-      link: "https://parmazon-prime.onrender.com/"
+      link: "https://parmazon-prime.onrender.com/",
+      gitlink: "https://github.com/sophie97yang/Parmazon-Prime"
     },
     {
       id: 2,
@@ -25,7 +26,8 @@ const ProjectSection = () => {
       description: "A philosophical debate platform built with a PostgreSQL Flask backend and a React frontend. Users can create and join debates on a variety of topics, and engage in thoughtful discussions with other users. There is only one topic per day to keep a focused topic and prevent doom-scrolling.",
       technologies: ["React", "Flask", "SQL"],
       image: "/PhilosophizeImg.png",
-      link: "https://philosophize.onrender.com/"
+      link: "https://philosophize.onrender.com/",
+      gitlink: "https://github.com/yoslatif/philosophize"
     },
     {
       id: 3,
@@ -33,7 +35,8 @@ const ProjectSection = () => {
       description: "AirBnb clone built using Node JS, React, and SQL. Project includes a strong demonstration of authentication features, booking, and review features, and a sleek design which imitates AirBnb's UI (one of my personal favorites) as close as possible.",
       technologies: ["NodeJS", "React"],
       image: "/yosbnbimg.png",
-      link: "https://yosauthme.onrender.com/"
+      link: "https://yosauthme.onrender.com/",
+      gitlink: "https://github.com/yoslatif/AirBnb"
     }
     // Add more projects as needed
   ];
@@ -56,8 +59,12 @@ const ProjectSection = () => {
               </div>
               <h3 className="text-lg font-bold">{project.title}</h3>
               <p className="text-sm text-gray-600">{project.description}</p>
-              <a href={project.link} className="visit-project-btn text-blue-500 mt-2 inline-block">
+              <a href={project.link} className="visit-project-btn text-blue-500 mt-3 inline-block">
                 Visit Project
+              </a>
+              <br />
+              <a href={project.gitlink} className="visit-project-btn text-blue-500 mt-3 inline-block">
+                GitHub Repo
               </a>
             </CardItem>
           </CardBody>
@@ -67,7 +74,7 @@ const ProjectSection = () => {
         .visit-project-btn:hover {
           box-shadow: 0 0 8px 4px rgba(0, 120, 255, 0.5); 
           transition: box-shadow 0.3s ease-in-out;
-          padding: 0.3rem;
+          padding: 0.4rem;
           border-radius: 0.9rem;
           // background-color: rgba(0, 120, 255, 0.1);
           background-color: pink;
